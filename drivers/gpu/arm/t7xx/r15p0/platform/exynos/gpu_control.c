030f01082083205e842d1f0f74963c99335b1848
skye-tachyon <sonicgames772@gmail.com>
2026-03-29 12:35:40 -0300

    Revert "drivers: gpu: arm: exynos7420: OC"
    This reverts commit c58a9a9ac9517cb1e4b3395d1363b08012aa4fb3.

--- drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_control.c
+++ drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_control.c
@@ -41,9 +41,6 @@
 
 static struct gpu_control_ops *ctr_ops;
 
-unsigned int gpu_min_override = 350;
-unsigned int gpu_max_override = 850;
-
 #ifdef CONFIG_MALI_RT_PM
 static struct exynos_pm_domain *gpu_get_pm_domain(void)
 {
