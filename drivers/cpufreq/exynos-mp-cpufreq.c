d562af9c3239c1296a6c3c08583b7c441ed9bcbe
skye-tachyon <sonicgames772@gmail.com>
2026-03-29 21:27:09 -0300

    cpufreq: push little to 2.0ghz, big to 2.49ghz
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -68,8 +68,8 @@
 #endif
 
 #ifdef CONFIG_SOC_EXYNOS7420
-#define CL0_MAX_VOLT		1175000
-#define CL1_MAX_VOLT		1125000
+#define CL0_MAX_VOLT		1250000
+#define CL1_MAX_VOLT		1250000
 #define CL0_MIN_VOLT		500000
 #define CL1_MIN_VOLT		500000
 #define CL_MAX_VOLT(cl)		(cl == CL_ZERO ? CL0_MAX_VOLT : CL1_MAX_VOLT)
@@ -81,9 +81,9 @@
 
 #ifdef CONFIG_SOC_EXYNOS7420
 #define CL0_MIN_FREQ		400000
-#define CL0_MAX_FREQ		1500000
+#define CL0_MAX_FREQ		2000000
 #define CL1_MIN_FREQ		300000
-#define CL1_MAX_FREQ		2100000
+#define CL1_MAX_FREQ		2496000 // atlas
 #else
 #error "Please define core frequency ranges for current SoC."
 #endif
