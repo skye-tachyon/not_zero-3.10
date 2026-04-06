3f42955fb66b26d7a87dc4f2bfa93dd6b86e92c5
skye-tachyon <sonicgames772@gmail.com>
2026-04-06 02:37:53 -0300

    cpufreq: limit oc
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- drivers/cpufreq/exynos7420-apollo-cpufreq.c
+++ drivers/cpufreq/exynos7420-apollo-cpufreq.c
@@ -294,16 +294,16 @@ static void __init set_volt_table_CA53(void)
 	case 0 :
 	case 1 :
 	case 4 :
-		max_support_idx_CA53 = L0; break;
+		max_support_idx_CA53 = L3; break;
 	case 5 :
-		max_support_idx_CA53 = L0; break;
+		max_support_idx_CA53 = L3; break;
 	case 12 :
-		max_support_idx_CA53 = L0; break;
+		max_support_idx_CA53 = L3; break;
 	default :
-		max_support_idx_CA53 = L0;
+		max_support_idx_CA53 = L3;
 	}
 
-	min_support_idx_CA53 = L18;	/* 200MHz */
+	min_support_idx_CA53 = L16;	/* 400MHz */
 	pr_info("CPUFREQ of CA53 max_freq : L%d %u khz\n", max_support_idx_CA53,
 		exynos7420_freq_table_CA53[max_support_idx_CA53].frequency);
 	pr_info("CPUFREQ of CA53 min_freq : L%d %u khz\n", min_support_idx_CA53,
